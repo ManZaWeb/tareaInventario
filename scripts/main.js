bf0480f7b7821915bcaf1ff48f98770b4b4ddbaa
Mario <maalnu93@hotmail.com>
2023-10-20 10:54:49 +0200

    Estructura Jss

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+import {listProducts} from "./listProducts.js"
+
+
+/**
+ * Autor Mario Alfonso Nuñez(DAW Student)
+ * Git:
+ */
+
+//Listamos los productos
+
+document.addEventListener("DOMContentLoaded", listProducts)
